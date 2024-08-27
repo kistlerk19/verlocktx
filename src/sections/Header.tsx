07d51133ebd08391c3851eae4f13ddafd74392be
Ishmael Gyamfi <isgyamfi25@gmail.com>
2024-08-27 16:53:29 +0000

    set up

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,10 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+  return <div className="flex justify-center items-center relative top-3">
+    <nav>
+      <a href="#">Home</a>
+      <a href="#skills">Projects</a>
+      <a href="#about">About</a>
+      <a href="#contact">Contact</a>
+    </nav>
+  </div>;
 };
